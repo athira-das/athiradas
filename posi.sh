f3e22c80062e60d40ef08b1e9d4686e8aad2f2ef
athira-das <athiradas9822@gmail.com>
2019-04-04 20:14:58 +0530

    posi commited

---
+++ posi.sh
@@ -0,0 +1,26 @@
+echo "Enter Num1"
+read n1
+if [ $n1 -lt 0 ]
+then
+echo "Number is negative"
+elif [ $n1 -gt 0 ]
+then
+echo "Number is positive"
+else
+echo "Neither positive nor negative"
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
